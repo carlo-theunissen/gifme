2a522f14a9174a0d8cdb16221a0cfcb1965f8077
Carlo THeunissen <carlotheunissen@Carlos-MacBook-Pro.local>
2017-05-28 17:27:45 +0200

    PreDeploy of php changes directory

--- php/_deploy/PreDeploy.sh
+++ php/_deploy/PreDeploy.sh
@@ -2,10 +2,7 @@
 # this script is executed on the deploy server, sysops
 # ==============
 
-#reset to head
-git checkout -- .
-
-git pull
+cd php
 
 #store the timestamp
 TIMESTAMP=$(date +%s)
@@ -13,18 +10,18 @@ TIMESTAMP=$(date +%s)
 #These files are zipped and sent to the servers for deployment
 #zip the files:
 zip -r $TIMESTAMP.zip \
-    ./php/app/* \
-    ./php/_deploy/AfterInstall.sh \
-    ./php/_deploy/ApplicationStart.sh \
-    ./php/_deploy/BeforeInstall.sh \
-    ./php/_deploy/user.ini \
-    ./php/_deploy/httpd.conf \
-    ./php/html/* \
-    ./php/html/.htaccess \
-    ./php/src/* \
-    ./php/composer.json \
-    ./php/composer.lock \
-    ./php/appspec.yml
+    ./app/* \
+    ./_deploy/AfterInstall.sh \
+    ./_deploy/ApplicationStart.sh \
+    ./_deploy/BeforeInstall.sh \
+    ./_deploy/user.ini \
+    ./_deploy/httpd.conf \
+    ./html/* \
+    ./html/.htaccess \
+    ./src/* \
+    ./composer.json \
+    ./composer.lock \
+    ./appspec.yml
 
 #send to s3
 aws s3 cp $TIMESTAMP.zip s3://gifcreatoruat/deploy/$TIMESTAMP.zip --region eu-central-1
@@ -37,3 +34,5 @@ aws deploy create-deployment \
  --application-name gifcreator_uat \
  --deployment-group-name php \
  --s3-location bucket=gifcreatoruat,bundleType=zip,key=deploy/$TIMESTAMP.zip --region us-east-1
+
+ cd ..
