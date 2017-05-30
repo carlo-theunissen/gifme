5ccd50e68e97448a816ae872460ee5c5f741f56b
Carlo THeunissen <carlotheunissen@carlos-mbp.home>
2017-05-31 00:49:39 +0200

    let apache and nodejs work together

--- node/Server.js
+++ node/Server.js
@@ -8,7 +8,7 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var server = http.createServer(function(request, response) {
 });
-server.listen(3000, function() { });
+server.listen(3001, function() { });
 
 
 
