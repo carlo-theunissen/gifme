82835c88cc90022a38d810da628d2d4bc97e4bdb
Carlo THeunissen <carlotheunissen@Carlos-MacBook-Pro.local>
2017-06-18 14:51:25 +0200

    remove invalid string in yml

--- php/_deploy/SetUserRights.sh
+++ php/_deploy/SetUserRights.sh
@@ -4,7 +4,7 @@
 chown -R codedeploy:webservices /var/www
 chown -R apache:webservices /var/www/var
 chmod 2755 -R /var/www #give user full rights, and others read and execute
-chmod 2775 -R /var/www/var #give the whole group right for writing, reading and executing, othes read and execute
+chmod 0775 -R /var/www/var #give the whole group right for writing, reading and executing, othes read and execute
 find /var/www -type f -exec chmod 0744 -- {} +
 
 #set ffprobe rights
