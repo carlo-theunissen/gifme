d183f4fd8508fafac56d4af39fa1a7b4c313b11c
Carlo THeunissen <carlotheunissen@Carlos-MacBook-Pro.local>
2017-06-18 14:11:33 +0200

    permissions are set in script, not through appspec

--- php/_deploy/ApplicationStart.sh
+++ php/_deploy/ApplicationStart.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#set rights to apache
-chgrp apache /var/www
-chmod g+w /var/www
-
-
 #start the server
 service httpd start
 
---
+++ php/_deploy/SetUserRights.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#set rights in /var/www folder
+chown -R codedeploy:webservices /var/www
+chown -R apache:webservices /var/www/var
+chmod 2755 -R /var/www #give user full rights, and others read and execute
+chmod 2775 -R /var/www/var #give the whole group right for writing, reading and executing, othes read and execute
+find /var/www -type f -exec chmod 0744 -- {} +
+
+#set ffprobe rights
+chown apache:webservices /usr/local/share/ffprobe
+chmod 2550 /usr/local/share/ffprobe
