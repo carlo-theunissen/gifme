56f4433d24415080256dfb1c85ebe9db73488e02
Carlo Theunissen <carlo@mediamonks.com>
2017-06-20 18:30:15 +0200

    Set socket.gifme.eu as websocket

--- frontend/config/endPoints.ts
+++ frontend/config/endPoints.ts
@@ -3,7 +3,7 @@
  */
 
 let frontController = "https://"+window.location.hostname+"/"; //frontcontroller
-let websocket = "wss://gifcreator-uat-2115672870.us-east-1.elb.amazonaws.com/ws";
+let websocket = "wss://socket.gifme.eu/ws";
 if(location.href.indexOf("localhost") >= 0){
     frontController = window.location.protocol + "//localhost:8888/gifcreator/php/html/app_dev.php/";
 }
